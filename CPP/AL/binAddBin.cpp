aa3d82e0eeb9e57b998bfd275b89ca4156f96506
evensuzy <ku06042007@gmail.com>
2023-04-02 22:13:31 +0700

    updatefilebinAddBin

--- CPP/AL/binAddBin.cpp
+++ CPP/AL/binAddBin.cpp
@@ -2,41 +2,31 @@
 using namespace std;
 
 int main() {
-	string bin1 = "", bin2 = "", binSum, remainder = "1";
-	cin >> bin1 >> bin2;
+ifstream inp("binAddBin.inp");
+ofstream out;
+out.open("binAddBin.out");
+	
+	
+	string bin1 = "", bin2 = "", binSum = "";
+	char  adding;
+	inp >> bin1 >> bin2;
+	
 	if (bin1.length() < bin2.length()) {
 		binSum = bin1;
 		bin1 = bin2;
 		bin2 = binSum;
+		binSum = ""; 
 	}
-//	cout << bin1 << endl << bin2;
-	binSum = "";
-	while (bin1.length() != bin2.length()){
+	while (bin1.length() != bin2.length()) {
 		bin2 = "0" + bin2;
 	}
-/*
-1001011
-  10101
-1100000
-0011111*/
-	for (int i = bin1.length(); i >= 0; i --) {
-		if (bin1[i] == 1 && bin2[i] == 1) {
-			binSum = "0" + remainder + binSum;
-			remainder = 1;
-		} else if (bin1[i] == 0 && bin2[i] == 0) {
-			binSum = "0" + remainder + binSum;
-		} else {
-			if (remainder == 1) {
-				cout << 8;
-				binSum = "0" + binSum;
-				remainder = 1;
-			} else {
-				cout << 9;
-				binSum = "1" + binSum;
-				remainder = 0;
-			}
-		}
+	out << bin1 << endl << bin2;	
+	for (int i = bin1.length() - 1; i >= 0; i --) {
+		adding = char(bin1[i] + int(bin2[i])- 48);
+//		out << bin1[i] << " " << bin2[i] << endl;
+		out << adding << endl;
 	}
-	cout << binSum;	
-	return 0;
+inp.close();  
+out.close();
+	return 0;	
 }
